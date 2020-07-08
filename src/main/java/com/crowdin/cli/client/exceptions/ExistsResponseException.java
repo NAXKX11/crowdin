03cc4aa0fb12e3c567a39cdaf70e70910c734145
Andrii Bodnar <andrii.bodnar@crowdin.com>
2020-07-08 10:25:37 +0300

    remove other files

--- src/main/java/com/crowdin/cli/client/exceptions/ExistsResponseException.java
+++
@@ -1,4 +0,0 @@
-package com.crowdin.cli.client.exceptions;
-
-public class ExistsResponseException extends ResponseException {
-}
--- src/main/java/com/crowdin/cli/client/exceptions/WaitResponseException.java
+++
@@ -1,4 +0,0 @@
-package com.crowdin.cli.client.exceptions;
-
-public class WaitResponseException extends ResponseException {
-}
